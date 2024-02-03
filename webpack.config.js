b5242419a27a1b222005161a1468ba219de1b325
Casey <casey.haralson@gmail.com>
2024-02-03 10:01:35 -0700

    adding ability to build extensions for multiple browsers

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
+let browser = process.env.BROWSER || 'chrome';
+
 let options = {
   mode: process.env.NODE_ENV || 'development',
   entry: {
@@ -10,7 +12,7 @@ let options = {
     service_worker: path.join(__dirname, 'src', 'scripts', 'service_worker.ts'),
   },
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'build', browser),
     filename: 'scripts/[name].bundle.js',
     clean: true,
   },
@@ -27,8 +29,12 @@ let options = {
           from: 'src',
           to: '.',
           globOptions: {
-            ignore: ['**/*.ts'],
+            ignore: ['**/*.ts', '**/manifest.*.json'],
           },
+        },
+        {
+          from: path.resolve(__dirname, 'src', 'manifest.' + browser + '.json'),
+          to: 'manifest.json',
         },
       ],
     }),
